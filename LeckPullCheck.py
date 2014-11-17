b55183f96bafa62be75a4d78a7b25650337bdff1
Chris Christensen <cchristensen@llnw.com>
2014-11-16 21:11:57 -0700

    Switch usage of upstream github API interaction and demo issue comments.

--- LeckPullCheck.py
+++ LeckPullCheck.py
@@ -3,22 +3,40 @@
 # Loads config to connect to various repos pull-requests comments and validate
 #  based on different criteria.
 
-import github3
+import github
 import ConfigParser
 
 
 class LeckPullChecker:
-    def __init__(self, config='config.yml', reponame=None):
+    config = ConfigParser.ConfigParser()
+    gh = None
+    repo = 'default'
+
+    def __init__(self, configfile='config.ini', reponame=None):
         # Initialize connection from config
-        Config = ConfigParser.ConfigParser()
-        Config.read(config)
-        import pprint
-        pprint.pprint(Config.sections())
+        self.config.read(configfile)
+        self.repo = reponame if reponame else 'default'
+        self.gh = github.Github(base_url=self.config.get('default', 'github'), login_or_token=self.config.get(self.repo, 'token'))
 
     def check(self, pullnumber=None):
         # Run through tests pull-request acceptance
-        pass
+        import pprint
+        repo = self.gh.get_repo('cchristensen/salt-pillar')
+        pr = repo.get_pull(1)
+        #pr.create_issue_comment('create_issue_comment HELLO WORLD')
+        comments = pr.get_comments()
+        pprint.pprint(comments)
+        c = None
+        for c in comments:
+            print 'HI'
+            pprint.pprint(c)
+        issue_comments = pr.get_issue_comments()
+        for ic in issue_comments:
+            print 'H2'
+            pprint.pprint(ic)
+        return self
 
 if __name__ == '__main__':
     #Construct based on CLI args
-    LeckPullChecker('config.yml.dist')
+    lpc = LeckPullChecker('config.ini.dist')
+    lpc.check()
